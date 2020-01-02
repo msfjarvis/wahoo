f23a4763154e29fec3f3161a67dfe7bdde909c4a
Harsh Shandilya <msfjarvis@gmail.com>
2020-01-02 22:47:28 +0530

    treewide: Address misleading indentation warnings from updated Clang
    Mixed spaces and tabs are flagged as misleading indentation
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- drivers/power/supply/qcom/pmic-voter.c
+++ drivers/power/supply/qcom/pmic-voter.c
@@ -174,7 +174,7 @@ static char *get_client_str(struct votable *votable, int client_id)
 	if (client_id == -EINVAL)
 		return NULL;
 
-	 return votable->client_strs[client_id];
+	return votable->client_strs[client_id];
 }
 
 void lock_votable(struct votable *votable)
